b853d014cf1afd93f16ec0a045665956304166ba
wik-wav <w.yashcher@gmail.com>
2025-12-19 07:52:43 +0100

    Quartz sync: Dec 19, 2025, 7:52 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "https://wik-wav.github.io/lozenge-tessellation/",
+    baseUrl: "wik-wav.github.io/lozenge-tessellation/",
     ignorePatterns: ["private", "templates", ".obsidian", "Wip", "00_Templates"],
     defaultDateType: "modified",
     theme: {
